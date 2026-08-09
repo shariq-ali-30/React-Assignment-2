cdbac67ae4026fff7b0ac56df8cfef4931c79476
shariq-ali-30 <shariq3072007@gmail.com>
2026-08-09 17:32:45 +0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,32 +5,80 @@ import heroImage from './assets/hero-banner.png'
 function App() {
     return (
         <>
-            <div className="navbar">
-
-                <div className="logo">
-                    <img src={logo} alt="logo" />
-                </div>
-
-                <div className="nav-links">
-                    <a href="">Home</a>
-                    <a href="">Plants</a>
-                    <a href="">Categories</a>
-                    <a href="">About Us</a>
-                    <a href="">Contact</a>
-                </div>
-
-                <div className="search-bar">
-                    <input type="text" placeholder="Search your favourite food..." />
-                </div>
-
-            </div>
-
             <div className="continer">
+
+                <div className="navbar">
+
+                    <div className="logo">
+                        <img src={logo} alt="logo" />
+                    </div>
+
+                    <div className="nav-links">
+                        <a href="">Home</a>
+                        <a href="">Plants</a>
+                        <a href="">Categories</a>
+                        <a href="">About Us</a>
+                        <a href="">Contact</a>
+                    </div>
+
+                    <div className="search-bar">
+                        <input type="text" placeholder="Search your favourite food..." />
+                    </div>
+
+                </div>
 
                 <section className="hero-section">
                     <img src={heroImage} alt="hero image" />
                 </section>
-                
+
+                <section className="cards-section">
+
+                    <div className="section-title">
+                        <p>Popular Foods</p>
+                        <h2>Most Loved dishes</h2>
+                        <span></span>
+                    </div>
+
+                    <div className="cards">
+                        <div className="card">
+                            <div className="image">
+                                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLQ89CA9ka1UCO7Jj2yPGeCfcWBnGzae5Q-SdGV1E1_A&s=10" alt="" />
+                            </div>
+                            <div className="details">
+                                <h3>Cheezy Margherita pizza</h3>
+                                <p> 4.6</p>
+                            </div>
+                        </div>
+                    </div>
+
+                </section>
+
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+
             </div>
         </>
     )
