56e409a5e7d82af63d3e9b924132f3a1e5751f93
shariq-ali-30 <shariq3072007@gmail.com>
2026-08-10 13:32:24 +0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -14,14 +14,15 @@ function App() {
                     </div>
 
                     <div className="nav-links">
-                        <a href="">Home</a>
-                        <a href="">Plants</a>
-                        <a href="">Categories</a>
-                        <a href="">About Us</a>
-                        <a href="">Contact</a>
+                        <div><a href="" className="active">Home <span></span></a></div>
+                        <div><a href="">Plants <span></span></a></div>
+                        <div><a href="">Categories <span></span></a></div>
+                        <div><a href="">About Us <span></span></a></div>
+                        <div><a href="">Contact <span></span></a></div>
                     </div>
 
                     <div className="search-bar">
+                        <i class="ph-bold ph-magnifying-glass"></i>
                         <input type="text" placeholder="Search your favourite food..." />
                     </div>
 
@@ -40,6 +41,7 @@ function App() {
                     </div>
 
                     <div className="cards">
+
                         <div className="card">
                             <div className="image">
                                 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLQ89CA9ka1UCO7Jj2yPGeCfcWBnGzae5Q-SdGV1E1_A&s=10" alt="" />
@@ -53,9 +55,46 @@ function App() {
                                 </div>
                             </div>
                         </div>
+                        <div className="card">
+                            <div className="image">
+                                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLQ89CA9ka1UCO7Jj2yPGeCfcWBnGzae5Q-SdGV1E1_A&s=10" alt="" />
+                            </div>
+                            <div className="details">
+                                <h3>Cheezy Margherita pizza</h3>
+                                <p><i class="ph-fill ph-star"></i> 4.6</p>
+                                <div>
+                                    <p>Rs. 180</p>
+                                    <span><i class="ph-bold ph-plus"></i></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <div className="image">
+                                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLQ89CA9ka1UCO7Jj2yPGeCfcWBnGzae5Q-SdGV1E1_A&s=10" alt="" />
+                            </div>
+                            <div className="details">
+                                <h3>Cheezy Margherita pizza</h3>
+                                <p><i class="ph-fill ph-star"></i> 4.6</p>
+                                <div>
+                                    <p>Rs. 180</p>
+                                    <span><i class="ph-bold ph-plus"></i></span>
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
 
                 </section>
+
+                <scetion className="wcu-section">
+
+                    <div className="section-title">
+                        <p>Why Choose Us</p>
+                        <h2>We Make It Easy</h2>
+                        <span></span>
+                    </div>
+
+                </scetion>
 
                 <br />
                 <br />
